ef82ce7bbd8c9039e62ac26aae35236cdca2e614
emmalynnn97 <emmalynnn97@gmail.com>
2020-11-05 23:19:29 -0700

    pls

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
-require('dotenv').config()
+require('dotenv/config')
 
 const app = express();
 
